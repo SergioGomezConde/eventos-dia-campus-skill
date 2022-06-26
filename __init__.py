1c2eb1260898704ff87482070b58d1bf6f14bd2a
serggom <sergio.s.gomezconde@gmail.com>
2022-06-26 22:07:59 +0200

    avances

--- __init__.py
+++ __init__.py
@@ -182,9 +182,14 @@ class EventosDiaCampus(MycroftSkill):
             # Lectura de la informacion del fichero JSON
             with open(ficheroJSON) as ficheroEventos:
                 data = json.load(ficheroEventos)
-                self.speak("El " + str(numero_dia) + " de " + dia_response.split(" ")[3] + " del " + str(numero_anio) + " tienes " + str(len(data['eventos'])) + " eventos")
-                for event in data['eventos']:
-                    self.speak("A las " + event['hora'] + " tienes " + event['nombre'])
+
+                if len(data['eventos']) == 0:
+                    self.speak("El " + str(numero_dia) + " de " + dia_response.split(" ")[3] + " del " + str(numero_anio) + " no tienes ningún evento")
+
+                else:
+                    self.speak("El " + str(numero_dia) + " de " + dia_response.split(" ")[3] + " del " + str(numero_anio) + " tienes " + str(len(data['eventos'])) + " eventos")
+                    for event in data['eventos']:
+                        self.speak("A las " + event['hora'] + " tienes " + event['nombre'])
 
             # # Obtencion del numero de eventos del dia
             # numero_eventos = len(eventos_dia)
